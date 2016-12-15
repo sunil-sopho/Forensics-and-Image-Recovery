c916b7ef196ba4c9bb90443d4041cccd2b1c746f
sunil kumar <sunilssaharan@gmail.com>
2016-12-15 18:25:41 +0000

    made a system for initials of jpgs and a pointer to output file

--- jpg/recover.c
+++ jpg/recover.c
@@ -19,5 +19,17 @@ int main(int argc, char* argv[])
         return 1;
     }
     
+    // jpg signatures are 8 bit integer
+    uint8_t chkjpg1[4]={0xff,0xd8,0xff,0xe0};
+    unit8_t chkjpg2[4]={0xff,0xd8,0xff,0xe01};
+    
+    //a counter to check jpg number
+    int count=0;
+    
+    // initialise a pointer for output file
+    FILE* outf;
+    
+    
+    
     
 }
