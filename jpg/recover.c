55792cd792426b9a224f91e7b603c4328fbc3e93
sunil kumar <sunilssaharan@gmail.com>
2016-12-16 13:00:36 +0000

    indented the code to pass style50

--- jpg/recover.c
+++ jpg/recover.c
@@ -6,18 +6,18 @@
  *
  * Recovers JPEGs from a forensic image.
  */
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdint.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
 
-int main()
+int main(void)
 {
     // opening the card file 
     FILE* inptr = fopen("card.raw","r");
     
     // checking if the file opened correctly
-    if(inptr==NULL)
+    if (inptr == NULL)
     {
         printf("couldn't open card.raw");
         return 1;
@@ -27,7 +27,7 @@ int main()
     uint8_t chkjpg1[4]={0xff,0xd8,0xff,0xe0};
     uint8_t chkjpg2[4]={0xff,0xd8,0xff,0xe1};
     
-    //a counter to check jpg number
+    // a counter to check jpg number
     int count=0;
     
     // initialise a pointer for output file
@@ -36,10 +36,10 @@ int main()
     // buffer created
     uint8_t buf[512];
     
-    //char title[8]; it should be iitaialise some where else
+    // char title[8]; it should be iitaialise some where else
     
     // start reading 512 bytes chunks 
-    for(int i=0;fread(buf,512,1,inptr)>0;i++)
+    for(int i = 0; fread(buf,512,1,inptr) > 0; i++)
     {
         // if we read eof then close infile and outfile
         // if((char)buf[0]==EOF)
@@ -50,13 +50,14 @@ int main()
         }*/
         
         // check if the read starting of new jpg
-        if(memcmp(buf,chkjpg1,4)==0||memcmp(buf,chkjpg2,4)==0)
+        if (memcmp(buf,chkjpg1,4) == 0 || memcmp(buf,chkjpg2,4) == 0)
         {
             char title[8];
             sprintf(title, "%03d.jpg",count);
             
-            // if a file is already opened then close that and open a new file esle open a file
-            if(outf)
+            // if a file is already opened then close that and
+            // open a new file esle open a file
+            if (outf)
             {
                 fclose(outf);
                 count++;
@@ -66,22 +67,24 @@ int main()
             else
             {
                 count++;
-                outf=fopen(title,"w");
+                outf = fopen(title,"w");
                 fwrite(buf,512,1,outf);
             }
         }
         else
         {
-            if(outf)
-            fwrite(buf,512,1,outf);
+            if (outf)
+            {
+                fwrite(buf,512,1,outf);
+            }
         }
     }
     // closeing all open files 
-    if(outf)
+    if (outf)
     {
         fclose(outf);
     }
-    if(inptr)
+    if (inptr)
     {
         fclose(inptr);
     }
