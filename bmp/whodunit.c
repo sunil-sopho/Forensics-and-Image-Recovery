89c7088ccfbc7d6dbc3e750506d2991ab64080ac
sunil kumar <sunilssaharan@gmail.com>
2016-12-16 13:04:44 +0000

    indented code for style50 pass

--- bmp/whodunit.c
+++ bmp/whodunit.c
@@ -81,10 +81,11 @@ int main(int argc, char* argv[])
             // read RGB triple from infile
             fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
             
-            if(triple.rgbtBlue==0x00&&triple.rgbtGreen==0x00&&triple.rgbtRed==0xff)
+            if (triple.rgbtBlue == 0x00 && triple.rgbtGreen == 0x00
+            && triple.rgbtRed == 0xff)
             {
-                triple.rgbtBlue=0xff;
-                triple.rgbtGreen=0xff;
+                triple.rgbtBlue = 0xff;
+                triple.rgbtGreen = 0xff;
             }
 
             // write RGB triple to outfile
